da235e8ba8214ffb14c44cb5a6e62d4748b344f6
Yunki-H <yunki.dev@gmail.com>
2023-08-02 01:08:26 +0900

    [Landmarks] Filter the List View

--- Landmarks/Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -22,9 +22,6 @@ struct LandmarkRow: View {
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
                     .foregroundStyle(.yellow)
-            } else {
-                Image(systemName: "star")
-                    .foregroundStyle(.yellow)
             }
         }
     }
